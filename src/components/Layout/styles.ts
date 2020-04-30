3f7edb69a92bbccd335ad09ac1d6821cb20919f0
Mateus Santana <mateuss.dev@gmail.com>
2020-04-30 19:32:27 -0300

    feature: add layout component

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.div.attrs(() => ({
+  'data-testid': 'zoe-layout'
+}))`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Middle = styled.section.attrs(() => ({
+  'data-testid': 'zoe-layout-middle'
+}))`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  min-height: calc(100vh - 160px);
+`
