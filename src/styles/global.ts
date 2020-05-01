ff5814c0d6cd813bf746dcd8e4bad62dd0b89979
Mateus Santana <mateuss.dev@gmail.com>
2020-05-01 20:28:14 -0300

    style: update global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,10 +6,12 @@ export default createGlobalStyle`
   ${normalize()}
 
   * {
+    margin: 0;
+    padding: 0;
     box-sizing:  border-box;
   }
 
-  body{
+  body {
     font-family: 'Nunito', sans-serif;
     font-size: 16px;
     background-color: ${(props): string => props.theme.colors.background}
