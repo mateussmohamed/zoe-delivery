d26305e79fb45ccd9708f9f73ef0ccedff13ef86
Mateus Santana <mateuss.dev@gmail.com>
2020-05-01 20:25:29 -0300

    test: add use-places-autocomplete result mocks

---
+++ src/__tests__/__mocks__/geoCodeResult.ts
@@ -0,0 +1,57 @@
+export default [
+  {
+    access_points: [],
+    address_components: [
+      {
+        long_name: 'Rua Major Sertório',
+        short_name: 'R. Maj. Sertório',
+        types: ['route']
+      },
+      {
+        long_name: 'Vila Buarque',
+        short_name: 'Vila Buarque',
+        types: ['political', 'sublocality', 'sublocality_level_1']
+      },
+      {
+        long_name: 'São Paulo',
+        short_name: 'São Paulo',
+        types: ['administrative_area_level_2', 'political']
+      },
+      {
+        long_name: 'São Paulo',
+        short_name: 'SP',
+        types: ['administrative_area_level_1', 'political']
+      },
+      {
+        long_name: 'Brazil',
+        short_name: 'BR',
+        types: ['country', 'political']
+      }
+    ],
+    formatted_address: 'R. Maj. Sertório - Vila Buarque, São Paulo - SP, Brazil',
+    geometry: {
+      bounds: {
+        south: -23.5455384,
+        west: -46.6521762,
+        north: -23.5452596,
+        east: -46.6446491
+      },
+      location: {
+        lat: -23.5454401,
+        lng: -46.6479462
+      },
+      location_type: 'GEOMETRIC_CENTER',
+      viewport: {
+        south: -23.5467479802915,
+        west: -46.6521762,
+        north: -23.5440500197085,
+        east: -46.6446491
+      }
+    },
+    place_id:
+      'EjlSLiBNYWouIFNlcnTDs3JpbyAtIFZpbGEgQnVhcnF1ZSwgU8OjbyBQYXVsbyAtIFNQLCBCcmF6aWwiLiosChQKEgnxPb75SFjOlBFNdlK4jZAQqBIUChIJbSABn0dYzpQRrAOmr4NXW70',
+    types: ['route'],
+    partial_match: true,
+    postcode_localities: ['99923232']
+  }
+]
