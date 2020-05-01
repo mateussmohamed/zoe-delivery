7fc7cfa0831bc4675f2ab2d86c852f0e8ff7d93c
Mateus Santana <mateuss.dev@gmail.com>
2020-04-30 21:12:30 -0300

    style: update global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,8 +5,13 @@ import { normalize } from 'polished'
 export default createGlobalStyle`
   ${normalize()}
 
+  * {
+    box-sizing:  border-box;
+  }
+
   body{
     font-family: 'Nunito', sans-serif;
+    font-size: 16px;
     background-color: ${(props): string => props.theme.colors.background}
   }
   `
