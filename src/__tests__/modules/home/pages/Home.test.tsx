ceeeea10dd547bd7760b1183916aab788f7cfcbd
Mateus Santana <mateuss.dev@gmail.com>
2020-05-02 21:34:46 -0300

    feature: add selected address

--- src/__tests__/modules/home/pages/Home.test.tsx
+++ src/__tests__/modules/home/pages/Home.test.tsx
@@ -1,42 +1,139 @@
 import React from 'react'
-import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
 import * as usePlacesAutocomplete from 'use-places-autocomplete'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { createMockClient, MockApolloClient } from 'mock-apollo-client'
 
-import { render } from 'test-utils'
+import { render, fireEvent, waitFor } from 'test-utils'
 
 import Home from '~/modules/home/pages/Home'
+import POC_SEARCH_QUERY from '~/modules/home/graphql/pocSearch'
+
+import typeResult from '~/__tests__/__mocks__/typeResult'
+import geoCodeResult from '~/__tests__/__mocks__/geoCodeResult'
 
 jest.mock('use-places-autocomplete')
 
 const mockedUsePlacesAutcomplete = usePlacesAutocomplete as jest.Mocked<typeof usePlacesAutocomplete>
+const clearSuggestionsMock = jest.fn()
+const setValueMock = jest.fn()
 
-const history = createMemoryHistory()
-
-mockedUsePlacesAutcomplete.default.mockReturnValue({
-  ready: false,
-  value: '',
-  suggestions: {
-    loading: false,
-    status: '',
-    data: []
-  },
-  setValue: jest.fn(),
-  clearSuggestions: jest.fn()
-})
+console.error = jest.fn()
 
 describe('<Home />', () => {
-  test('renders correctly', async () => {
-    const { getByTestId } = render(
-      <Router history={history}>
-        <Home />
-      </Router>
-    )
+  describe('when customer type a address', () => {
+    let mockClient: MockApolloClient
 
-    const header = getByTestId('zoe-header')
-    const footer = getByTestId('zoe-footer')
+    afterEach(() => jest.restoreAllMocks())
+    beforeEach(() => {
+      // @ts-ignore
+      mockedUsePlacesAutcomplete.getGeocode.mockResolvedValue(geoCodeResult)
+      mockedUsePlacesAutcomplete.getLatLng.mockResolvedValue({
+        lat: geoCodeResult[0].geometry.location.lat,
+        lng: geoCodeResult[0].geometry.location.lng
+      })
+      mockedUsePlacesAutcomplete.default.mockReturnValue({
+        setValue: setValueMock,
+        clearSuggestions: clearSuggestionsMock,
+        ready: true,
+        value: 'rua americo brasiliense',
+        suggestions: {
+          loading: false,
+          status: 'OK',
+          data: typeResult.data
+        }
+      })
 
-    expect(header).toHaveTextContent('Zoe')
-    expect(footer).toHaveTextContent('Zoe')
+      mockClient = createMockClient()
+
+      jest.spyOn(Date.prototype, 'toISOString').mockReturnValue('2020-05-02T22:27:44.795Z')
+    })
+
+    describe('when customer selected an available address to delivery', () => {
+      test('it should render <Button /> to search products', async () => {
+        const returnQuery = {
+          loading: false,
+          data: {
+            pocSearch: [
+              {
+                id: '532',
+                status: 'AVAILABLE',
+                tradingName: 'Distribuidor de Treinamento',
+                officialName: 'Distribuidor de Treinamento'
+              }
+            ]
+          }
+        }
+
+        const pocSearchHandler = jest.fn().mockResolvedValue(returnQuery)
+
+        mockClient.setRequestHandler(POC_SEARCH_QUERY, pocSearchHandler)
+
+        const { findAllByTestId, findByTestId } = render(
+          <ApolloProvider client={mockClient}>
+            <Home />
+          </ApolloProvider>
+        )
+
+        const suggestions = await findAllByTestId('zoe-address-suggestions-item')
+
+        fireEvent.click(suggestions[0])
+
+        await waitFor(async () => {
+          const button = await findByTestId('zoe-button-search-products')
+
+          expect(button).toBeTruthy()
+          expect(pocSearchHandler).toBeCalledTimes(1)
+          expect(pocSearchHandler).toBeCalledWith({
+            lat: '-23.5454401',
+            long: '-46.6479462',
+            algorithm: 'NEAREST',
+            now: '2020-05-02T22:27:44.795Z'
+          })
+        })
+      })
+    })
+
+    describe('when customer type an available address to delivery', () => {
+      test('it should render <UnavailableService /> message', async () => {
+        mockedUsePlacesAutcomplete.getLatLng.mockResolvedValue({
+          lat: -99.434343,
+          lng: -343.4343
+        })
+
+        const returnQuery = {
+          loading: false,
+          data: {
+            pocSearch: []
+          }
+        }
+
+        const pocSearchHandler = jest.fn().mockResolvedValue(returnQuery)
+
+        mockClient.setRequestHandler(POC_SEARCH_QUERY, pocSearchHandler)
+
+        const { findAllByTestId, findByTestId } = render(
+          <ApolloProvider client={mockClient}>
+            <Home />
+          </ApolloProvider>
+        )
+
+        const suggestions = await findAllByTestId('zoe-address-suggestions-item')
+
+        fireEvent.click(suggestions[0])
+
+        await waitFor(async () => {
+          const unavailable = await findByTestId('zoe-unavailable')
+
+          expect(unavailable).toBeTruthy()
+          expect(pocSearchHandler).toBeCalledTimes(1)
+          expect(pocSearchHandler).toBeCalledWith({
+            lat: '-99.434343',
+            long: '-343.4343',
+            algorithm: 'NEAREST',
+            now: '2020-05-02T22:27:44.795Z'
+          })
+        })
+      })
+    })
   })
 })
--- src/modules/home/pages/Home/index.tsx
+++ src/modules/home/pages/Home/index.tsx
@@ -1,15 +1,42 @@
 import React, { FC } from 'react'
+import { useLazyQuery } from '@apollo/react-hooks'
 
 import Layout from '../../../../components/Layout'
 import CustomerAddress from '../../components/CustomerAddress'
+import Loading from '../../../../components/Loading'
 
-import { Container } from './styles'
+import POC_SEARCH_QUERY, { TPocSearchData, TPocVariables } from '../../graphql/pocSearch'
+import UnavailableService from '../../components/UnavailableService'
+
+import { Container, Button } from './styles'
 
 const Home: FC = () => {
+  const [pocSearch, { loading, data }] = useLazyQuery<TPocSearchData, TPocVariables>(POC_SEARCH_QUERY)
+
+  const onSelectAddress = (lat: number, long: number): void => {
+    pocSearch({
+      variables: {
+        lat: `${lat}`,
+        long: `${long}`,
+        algorithm: 'NEAREST',
+        now: new Date().toISOString()
+      }
+    })
+  }
+
+  const availableService = data && data.pocSearch.length > 0
+  const unavailableService = data?.pocSearch.length === 0
+
   return (
     <Layout>
       <Container>
-        <CustomerAddress />
+        <CustomerAddress onSelectAddress={onSelectAddress} />
+
+        {availableService && <Button>Buscar Produtos</Button>}
+
+        {unavailableService && <UnavailableService />}
+
+        {loading && <Loading />}
       </Container>
     </Layout>
   )
