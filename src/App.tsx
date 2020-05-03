680b50017ef699897cbf378f2e557972dc64c0ca
Mateus Santana <mateuss.dev@gmail.com>
2020-05-03 18:58:07 -0300

    feature: add app context

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { hot } from 'react-hot-loader/root'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { ThemeProvider } from 'styled-components'
 
-import client from './apollo/client'
-import Routes from './routes/Routes'
-import GlobalStyle from './styles/global'
-import theme from './styles/themes/default'
+import client from '~/apollo/client'
+
+import { ZoeProvider } from '~/context'
+import Routes from '~/routes/Routes'
+
+import GlobalStyle from '~/styles/global'
+import theme from '~/styles/themes/default'
 
 const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <ApolloProvider client={client}>
-        <Routes />
+        <ZoeProvider>
+          <Routes />
+        </ZoeProvider>
       </ApolloProvider>
     </ThemeProvider>
   )
